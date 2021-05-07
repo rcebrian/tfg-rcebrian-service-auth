a188b76a299bf5fbd1528b2ccee2b9c3e5c08978
rcebrian <rodrigocebriang@gmail.com>
2021-05-08 01:18:50 +0200

    :memo: roles controller documentation

--- src/api/controllers/roles.controller.ts
+++ src/api/controllers/roles.controller.ts
@@ -2,6 +2,11 @@ import httpStatus from 'http-status';
 import { Request, Response } from 'express';
 import { Role } from '../repository/mysql/mysql.repository';
 
+/**
+ * Get all roles
+ * @param req GET request
+ * @param res OK with all roles
+ */
 export const findAll = (req: Request, res: Response) => {
   Role.scope('full').findAll().then((data: any) => {
     res.status(httpStatus.OK)
