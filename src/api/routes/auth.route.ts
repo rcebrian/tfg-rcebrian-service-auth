f32db13c63e0bd80f26eb4c065efdfce349a9a42
rcebrian <rodrigocebriang@gmail.com>
2021-05-08 01:00:39 +0200

    :sparkles: change password endpoint

--- src/api/routes/auth.route.ts
+++ src/api/routes/auth.route.ts
@@ -1,13 +1,23 @@
 import { Router } from 'express';
 import { validatorHandler, auth } from '@rcebrian/tfg-rcebrian-common';
-import { signIn, refresh, signUp, signOut } from '../controllers/auth.controller';
-import { signUpValidation, signInValidation } from '../validations';
+import {
+  signIn, refresh, signUp, signOut, changePassword,
+} from '../controllers/auth.controller';
+import { signUpValidation, signInValidation, changePasswordValidation } from '../validations';
 
 const router = Router();
 
+/**
+ * NO AUTH NEEDED
+ */
 router.route('/signUp').post(signUpValidation(), validatorHandler, signUp);
 router.route('/signIn').post(signInValidation(), validatorHandler, signIn);
+
+/**
+ * NEED TO BE AUTHENTICATED
+ */
 router.route('/signOut').post(auth, signOut);
 router.route('/refresh').post(auth, refresh);
+router.route('/change-password').put(auth, changePasswordValidation(), validatorHandler, changePassword);
 
 export { router as authRoutes };
