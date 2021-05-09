7be26d1db1169802171a42873d58dc429196da19
rcebrian <rodrigocebriang@gmail.com>
2021-05-09 14:03:20 +0200

    :zap: signUp need auth & perform validations

--- src/api/routes/auth.route.ts
+++ src/api/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { validatorHandler, auth } from '@rcebrian/tfg-rcebrian-common';
+import { auth } from '@rcebrian/tfg-rcebrian-common';
 import {
   signIn, refresh, signUp, signOut, changePassword,
 } from '../controllers/auth.controller';
@@ -10,14 +10,14 @@ const router = Router();
 /**
  * NO AUTH NEEDED
  */
-router.route('/signUp').post(signUpValidation(), validatorHandler, signUp);
-router.route('/signIn').post(signInValidation(), validatorHandler, signIn);
+router.route('/signIn').post(signInValidation(), signIn);
 
 /**
  * NEED TO BE AUTHENTICATED
  */
-router.route('/signOut').post(auth, signOut);
+router.route('/signUp').post(auth, signUpValidation(), signUp);
 router.route('/refresh').post(auth, refresh);
-router.route('/change-password').put(auth, changePasswordValidation(), validatorHandler, changePassword);
+router.route('/signOut').post(auth, signOut);
+router.route('/change-password').put(auth, changePasswordValidation(), changePassword);
 
 export { router as authRoutes };
